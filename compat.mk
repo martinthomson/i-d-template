791a17112a2cec7ab10da38e7ab056a01076c2b9
Martin Thomson <martin.thomson@gmail.com>
2015-10-17 12:03:54 -0700

    Updating instructions

--- compat.mk
+++ compat.mk
@@ -1,12 +1,13 @@
 ifeq (3,$(word 1,$(subst ., ,$(MAKE_VERSION))))
-$(warning =======================)
+$(warning =================================================)
 $(warning GNU Make version $(MAKE_VERSION) isn't supported)
 $(warning You should upgrade to version 4)
 ifeq (Darwin,$(shell uname -s))
+$(warning -- OS X:)
 $(warning With homebrew (https://brew.sh) type:)
 $(warning $$ brew tap homebrew/dupes)
 $(warning $$ brew install homebrew/dupes/make)
-$(warning This installs `gmake`)
+$(warning Note: This installs make as `gmake`)
 endif
-$(warning =======================)
+$(warning =================================================)
 endif
