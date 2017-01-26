f3751755bcbc15ce8c66c9c2ebdae9ad688b48da
Martin Thomson <martin.thomson@gmail.com>
2017-01-26 16:27:00 +0900

    Missing semicolon (damn you macOS with your shitty sed)

--- setup.mk
+++ setup.mk
@@ -84,7 +84,7 @@ setup-markdown: $(firstword $(drafts)).xml $(MARKDOWN_FILES)
 	  WG_NAME=$$(echo $< | cut -f 3 -d - -); \
 	  DRAFT_STATUS=$$(test "$$AUTHOR_LABEL" = ietf && echo Working Group || echo Individual); \
 	  GITHUB_USER=$(GITHUB_USER); GITHUB_REPO=$(GITHUB_REPO); \
-	  DRAFT_TITLE=$$(sed -e '/<title[^>]*>/,/<\/title>/{s/.*<title[^>]*>//;/<\/title>/{s/<\/title>.*//;H;x;q};H;};d' $< | xargs echo); \
+	  DRAFT_TITLE=$$(sed -e '/<title[^>]*>/,/<\/title>/{s/.*<title[^>]*>//;/<\/title>/{s/<\/title>.*//;H;x;q;};H;};d' $< | xargs echo); \
 	  sed -i~ $(foreach label,DRAFT_NAME DRAFT_TITLE DRAFT_STATUS GITHUB_USER GITHUB_REPO WG_NAME,-e 's~{$(label)}~'"$$$(label)"'~g') $(MARKDOWN_FILES)
 	@-rm -f $(addsuffix ~,$(MARKDOWN_FILES))
 	git add $(MARKDOWN_FILES)
