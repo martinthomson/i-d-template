94f4d1c640067468711ad8b3d52998f95dd6ae8c
Martin Thomson <martin.thomson@gmail.com>
2017-03-09 16:00:04 +1100

    Generate a test report for circle

--- main.mk
+++ main.mk
@@ -127,6 +127,33 @@ draft_diffs := $(addprefix diff-,$(addsuffix .html,$(drafts_with_prev)))
 .PHONY: diff
 diff: $(draft_diffs)
 
+## Generate a test report
+ifneq (,$(CIRCLE_TEST_REPORTS))
+TEST_REPORT := $(CIRCLE_TEST_REPORTS)/report/drafts.xml
+else
+TEST_REPORT := report.xml
+endif
+.PHONY: report
+report: $(TEST_REPORT)
+$(TEST_REPORT): $(drafts_html) $(drafts_txt)
+	@echo build_report $^
+	@mkdir -p $(dir $@)
+	@echo '<?xml version="1.0" encoding="UTF-8"?>' >$@
+	@passed=();failed=();for i in $^; do \
+	  if [ -f "$$i" ]; then passed+=("$$i"); else failed+=("$$i"); fi; \
+	done; echo '<testsuite' >>$@; \
+	echo '    tests="'"$$(($${#passed[@]} + $${#failed[@]}))"'"' >>$@; \
+	echo '    failures="'"$${#failed[@]}"'">' >>$@; \
+	for i in "$${passed[@]}"; do \
+	  echo '  <testcase name="'"$$i"'" classname="build.'"$${i%.*}"'"/>' >>$@; \
+	done; \
+	for i in "$${failed[@]}"; do \
+	  echo '  <testcase name="'"$$i"'" classname="build.'"$${i%.*}"'">' >>$@; \
+	  echo '    <failure message="Error building file"/>' >>$@; \
+	  echo '  </testcase>' >>$@; \
+	done; \
+	echo '</testsuite>' >>$@
+
 ## Cleanup
 COMMA := ,
 .PHONY: clean
