martinthomson/i-d-template

commit 5b6bdb3697b9c65599de845eb1302df4e16f59c5

Remove -latest from references to other drafts (markdown only)

main.mk
@@ -17,24 +17,26 @@ pdf:: $(addsuffix .pdf,$(drafts))
 ## Basic Recipes
 .INTERMEDIATE: $(filter-out $(join $(drafts),$(draft_types)),$(addsuffix .xml,$(drafts)))
 
-ifdef MD_PREPROCESSOR
-.INTERMEDIATE: $(addsuffix .mdtmp,$(drafts))
-%.mdtmp: %.md
-	$(MD_PREPROCESSOR) < $< > $@
-
-%.xml: %.mdtmp
-else
-%.xml: %.md
+NOT_CURRENT = $(filter-out $(basename $<),$(drafts))
+ifneq (,$(MD_PREPROCESSOR))
+MD_PREPROCESSOR := | $(MD_PREPROCESSOR)
 endif
+ifneq (1,$(words $(drafts)))
+REMOVE_LATEST = | sed -e '$(join $(addprefix s/,$(addsuffix -latest/,$(NOT_CURRENT))), \
+		$(addsuffix /g;,$(NOT_CURRENT)))'
+else
+REMOVE_LATEST =
+endif
+export XML_RESOURCE_ORG_PREFIX
+
+%.xml: %.md
 	@h=$$(head -1 $< | cut -c 1-3 -); \
 	if [ "$$h" = '---' ]; then \
-	  echo XML_RESOURCE_ORG_PREFIX=$(XML_RESOURCE_ORG_PREFIX) \
-	    $(kramdown-rfc2629) $< \> $@; \
-	  XML_RESOURCE_ORG_PREFIX=$(XML_RESOURCE_ORG_PREFIX) \
-	    $(kramdown-rfc2629) $< > $@; \
+	  echo '$(subst ','"'"',cat $< $(MD_PREPROCESSOR) $(REMOVE_LATEST) | $(kramdown-rfc2629) $< > $@)'; \
+	  cat $< $(MD_PREPROCESSOR) $(REMOVE_LATEST) | $(kramdown-rfc2629) $< > $@; \
 	elif [ "$$h" = '%%%' ]; then \
-	  echo $(mmark) -xml2 -page $< $@; \
-	  $(mmark) -xml2 -page $< $@; \
+	  echo '$(subst ','"'"',cat $< $(MD_PREPROCESSOR) $(REMOVE_LATEST) | $(mmark) -xml2 -page > $@)'; \
+	  cat $< $(MD_PREPROCESSOR) $(REMOVE_LATEST) | $(mmark) -xml2 -page > $@; \
 	else \
 	  ! echo "Unable to detect '%%%' or '---' in markdown file" 1>&2; \
 	fi

commit b9d1ff2442338530ca355612e5b287f8fe4fbdbb

Fix preprocessing when kramdown-rfc2629 is used.
This fixes a bug introduced in commit
5b6bdb3697b9c65599de845eb1302df4e16f59c5
in which the preprocessor was run but kramdown-2629 actually read
input from the .md file not from stdin, which meant that the
preprocessed .md file was ignored.

main.mk
@@ -32,8 +32,8 @@ export XML_RESOURCE_ORG_PREFIX
 %.xml: %.md
 	@h=$$(head -1 $< | cut -c 1-3 -); \
 	if [ "$$h" = '---' ]; then \
-	  echo '$(subst ','"'"',cat $< $(MD_PREPROCESSOR) $(REMOVE_LATEST) | $(kramdown-rfc2629) $< > $@)'; \
-	  cat $< $(MD_PREPROCESSOR) $(REMOVE_LATEST) | $(kramdown-rfc2629) $< > $@; \
+	  echo '$(subst ','"'"',cat $< $(MD_PREPROCESSOR) $(REMOVE_LATEST) | $(kramdown-rfc2629) > $@)'; \
+	  cat $< $(MD_PREPROCESSOR) $(REMOVE_LATEST) | $(kramdown-rfc2629) > $@; \
 	elif [ "$$h" = '%%%' ]; then \
 	  echo '$(subst ','"'"',cat $< $(MD_PREPROCESSOR) $(REMOVE_LATEST) | $(mmark) -xml2 -page > $@)'; \
 	  cat $< $(MD_PREPROCESSOR) $(REMOVE_LATEST) | $(mmark) -xml2 -page > $@; \