8e25b6050c93043b54991de1ecbe3c58921dbcec
Martin Thomson <martin.thomson@gmail.com>
2017-05-26 18:47:21 +1000

    Fix ordering of args

--- setup-branch.sh
+++ setup-branch.sh
@@ -34,11 +34,11 @@ for f in "$@"; do
 done
 
 echo Commit and push to origin/"$branch"
-commit=(commit)
-git config --global --get user.name >/dev/null || commit+=(-c user.name='ID Bot')
-git config --global --get user.email >/dev/null || commit+=(-c user.email='idbot@example.com')
+user=()
+git config --global --get user.name >/dev/null || user+=(-c user.name='ID Bot')
+git config --global --get user.email >/dev/null || user+=(-c user.email='idbot@example.com')
 
 git -C "$tmp" add circle.yml .gitignore "$@"
-git -C "$tmp" "${commit[@]}" -m "Automatic setup of $branch."
+git -C "$tmp" "${user[@]}" commit -m "Automatic setup of $branch."
 git -C "$tmp" push origin "$branch"
 git push --set-upstream origin "$branch"
