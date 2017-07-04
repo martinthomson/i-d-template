547462cb69b3d6a6b6261acb0e2ad2801ad7259b
Martin Thomson <martin.thomson@gmail.com>
2017-07-04 14:21:49 +1000

    Add a lint step to the pre-commit

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,16 +2,24 @@
 
 exec 1>&2
 
-# No tests to update gh-pages
 BRANCH=$(git symbolic-ref --short HEAD 2>/dev/null)
 if [ "$BRANCH" = "gh-pages" -o "$BRANCH" = "gh-issues" -o -e .git/MERGE_HEAD ]; then
      exit
 fi
 
-files=($(git status --porcelain draft-* | sed '/^[MARCU]/{s/.*draft-/draft-/;p;};d'))
+hash gmake 2> /dev/null && MAKE=gmake || MAKE=make
+
+function abort() {
+    echo "Commit refused: documents don't build successfully."
+    echo "To commit anyway, run \"git commit --no-verify\""
+    exit 1
+}
+trap abort ERR
+
+files=($(git status --porcelain draft-* | sed '/^[MARCU]/{s/.*draft-/draft-/;p;};d' | sort))
 tmpfiles=()
 txtfiles=()
-trap 'rm -f "${tmpfiles[@]}" "${txtfiles[@]}"' ERR EXIT
+trap 'rm -f "${tmpfiles[@]}" "${txtfiles[@]}"' EXIT
 for f in "${files[@]}"; do
     tmp="${f%.*}"-tmp$$."${f##*.}"
     tmpfiles+=("$tmp")
@@ -19,12 +27,7 @@ for f in "${files[@]}"; do
     # This makes a copy of the staged file.
     git show :"$f" > "$tmp"
 done
-hash gmake 2> /dev/null && MAKE=gmake || MAKE=make
-[ "${#txtfiles[@]}" -eq 0 ] || "$MAKE" "${txtfiles[@]}"
-RESULT=$?
+[ "${#txtfiles[@]}" -eq 0 ] && exit 0
 
-if [ $RESULT -ne 0 ]; then
-  echo "Commit refused -- documents don't build successfully."
-  echo "To commit anyway, run \"git commit --no-verify\""
-  exit 1
-fi
+"$MAKE" "${txtfiles[@]}"
+"$MAKE" lint "drafts=${txtfiles[@]%.*}"
