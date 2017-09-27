592e3b4e041de5f610081eb5fe41793f5cd3b42a
Martin Thomson <martin.thomson@gmail.com>
2017-09-27 18:34:44 +1000

    Use the CI tag

--- upload.mk
+++ upload.mk
@@ -1,6 +1,10 @@
 # This only works for tags that are created with `git tag -a`.  Using an
 # annotated tag associates an email address, which this uses in the upload.
+ifneq (,$(CIRCLE_TAG)$(TRAVIS_TAG))
+draft_releases := $(CIRCLE_TAG)$(TRAVIS_TAG)
+else
 draft_releases := $(shell git tag --list --points-at HEAD --format '%(tag),%(taggeremail)' | grep '^draft-.*,<.*>$$' | cut -f 1 -d , -)
+endif
 uploads := $(addprefix .,$(addsuffix .upload,$(draft_releases)))
 
 .PHONY: upload
