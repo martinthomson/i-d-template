4ef77f96f02e65c6f87b09f15c32d9d863ebb69a
Martin Thomson <martin.thomson@gmail.com>
2018-03-28 10:26:38 +1100

    Check tag types and number

--- pre-push.sh
+++ pre-push.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 err=0
+tags=0
 while read local_ref local_sha remote_ref remote_sha; do
-    if [[ "${local_ref#refs/tags/draft-}" != "${local_ref}" && \
-          -z "$(git for-each-ref --format '%(taggeremail)' "${local_ref}")" ]]; then
-        echo "pre-push: tag ${local_ref#refs/tags/} is not an annotated tag" 1>&2
-	err=1
+    if [[ "${local_ref#refs/tags/draft-}" != "${local_ref}" ]]; then
+        if [[ -z "$(git for-each-ref --format '%(taggeremail)' "${local_ref}")" ]]; then
+            echo "pre-push: tag ${local_ref#refs/tags/} is not an annotated tag" 1>&2
+	    err=1
+	fi
+	tags=$((tags+1))
     fi
 done
-[[ $err -eq 0 ]] || echo "pre-push: Use git push --no-verify to override this check" 1>&2
+if [[ $tags -gt 1 ]]; then
+    echo "pre-push: more than one tag pushed, which circle doesn't currently handle" 1>&2
+    err=2
+fi
+[[ $err -eq 0 ]] || echo "pre-push: use git push --no-verify to override this check" 1>&2
 exit $err
