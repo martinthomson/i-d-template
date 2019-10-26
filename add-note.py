d3f66d433c733c22fd11c135d4cb567f68e84d2a
Martin Thomson <mt@lowentropy.net>
2019-10-26 12:24:22 +1100

    Reformat

--- add-note.py
+++ add-note.py
@@ -4,14 +4,14 @@ import fileinput
 import re
 
 found = False
-frontEnd = re.compile(' *</front>')
+frontEnd = re.compile(" *</front>")
 for line in fileinput.input():
     if not found:
         match = frontEnd.match(line)
         if match:
             found = True
             if match.start() > 0:
-                print(line[:match.start()], end=None)
+                print(line[: match.start()], end=None)
 
             try:
                 with fileinput.FileInput(files=(".note.xml")) as note:
@@ -20,11 +20,11 @@ for line in fileinput.input():
             except:
                 pass
 
-            print(line[match.start():], end=None)
+            print(line[match.start() :], end=None)
             continue
 
         # Don't add a note if there is one already.
-        if line.find('</note>') >= 0:
+        if line.find("</note>") >= 0:
             found = True
 
     print(line, end=None)
