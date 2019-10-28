adc5de540d50da5f1bd90f3341c48b37be4208c6
Martin Thomson <mt@lowentropy.net>
2019-10-28 20:47:48 +1100

    end=""

--- add-note.py
+++ add-note.py
@@ -11,20 +11,20 @@ for line in fileinput.input():
         if match:
             found = True
             if match.start() > 0:
-                print(line[: match.start()], end=None)
+                print(line[: match.start()], end="")
 
             try:
                 with fileinput.FileInput(files=(".note.xml")) as note:
                     for n in note:
-                        print(n, end=None)
+                        print(n, end="")
             except:
                 pass
 
-            print(line[match.start() :], end=None)
+            print(line[match.start() :], end="")
             continue
 
         # Don't add a note if there is one already.
         if line.find("</note>") >= 0:
             found = True
 
-    print(line, end=None)
+    print(line, end="")
