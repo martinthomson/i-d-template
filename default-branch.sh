f503b30ce5ecad7ddf6535775c61b5703110333e
Martin Thomson <mt@lowentropy.net>
2020-06-19 19:11:00 +1000

    Suppress that error

--- default-branch.sh
+++ default-branch.sh
@@ -8,7 +8,7 @@ if [[ "$branch" == "$origin/HEAD" ]]; then
     # If $origin doesn't exist, then the above rev-parse will fail, but that is
     # OK.  We can let git remote report the error.
     set -e
-    git remote set-head -a "$origin" 1>&2
+    git remote set-head -a "$origin" 1>&2 2>/dev/null
     branch=$(git rev-parse --abbrev-ref "$origin/HEAD" 2>/dev/null)
 fi
 echo "${branch##*/}"
