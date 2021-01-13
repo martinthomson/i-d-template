827122b0c7439c1dc10e959c6ad53c6c100c9078
Martin Thomson <mt@lowentropy.net>
2021-01-13 16:14:52 +1100

    Fix a11y for pilcrows
    This makes them visible always, but not emphasized so that they don't
    distract sighted readers.  This means that screen readers will have this
    bump always, but the feedback I received indicated that this was
    something that people would prefer to have access to, even if it adds a
    little extra noise.

--- config.mk
+++ config.mk
@@ -4,7 +4,9 @@
 
 # Mandatory:
 #   https://pypi.python.org/pypi/xml2rfc
-xml2rfc ?= xml2rfc -q -s 'Setting consensus="true" for IETF STD document'
+XML2RFC_RFC_BASE_URL := https://tools.ietf.org/html/
+XML2RFC_ID_BASE_URL := https://tools.ietf.org/html/
+xml2rfc ?= xml2rfc -q -s 'Setting consensus="true" for IETF STD document' --rfc-reference-base-url $(XML2RFC_RFC_BASE_URL) --id-reference-base-url $(XML2RFC_ID_BASE_URL)
 
 # If you are using markdown files use either kramdown-rfc2629 or mmark
 #   https://github.com/cabo/kramdown-rfc2629
