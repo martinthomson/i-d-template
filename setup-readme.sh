61604bf3a859de334e6dbb9ec353af93ae69d18a
Robin Marx <robin.marx@kuleuven.be>
2021-06-10 16:14:11 +0200

    Forgotten master rename to main

--- setup-readme.sh
+++ setup-readme.sh
@@ -83,7 +83,7 @@ $ make
 \`\`\`
 
 This requires that you have the necessary software installed.  See
-[the instructions](https://github.com/martinthomson/i-d-template/blob/master/doc/SETUP.md).
+[the instructions](https://github.com/martinthomson/i-d-template/blob/${default_branch}/doc/SETUP.md).
 
 
 ## Contributing
