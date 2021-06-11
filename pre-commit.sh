ea7cd9a5f41f9fb270983cbc31cd7e5df2cf3e8a
Mike Bishop <mbishop@evequefou.be>
2021-06-11 11:12:32 -0400

    Simpler
    Co-authored-by: Martin Thomson <mt@lowentropy.net>

--- pre-commit.sh
+++ pre-commit.sh
@@ -23,7 +23,7 @@ function abort() {
 trap abort ERR
 trap cleanup EXIT
 
-files=($(git status --porcelain draft-* rfc* | sed '/^[[:space:]]*[MARCU]/{s/[[:space:]]*[MARCU][[:space:]]*\(draft-\|rfc\)/\1/;p;};d' | sort))
+files=($(git status --porcelain draft-* rfc* | sed '/^[MARCU]/{s/.* \(draft-\|rfc\)/\1/;p;};d' | sort))
 for f in "${files[@]}"; do
     tmp="${f%.*}"-tmp$$."${f##*.}"
     tmpfiles+=("$tmp")
