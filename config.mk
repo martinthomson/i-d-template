915002b85542c686a0aa296d9ab5d217600b812b
Martin Thomson <mt@lowentropy.net>
2024-08-20 19:47:13 +1000

    Fixups

--- config.mk
+++ config.mk
@@ -28,7 +28,7 @@ export KRAMDOWN_NO_TARGETS := true
 export KRAMDOWN_PERSISTENT := true
 
 # Use an emoji for the favicon
-FAVICON_EMOJI :=
+FAVICON_EMOJI ?=
 ifneq (,$(FAVICON_EMOJI))
 FAVICON ?= <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>$(FAVICON_EMOJI)</text></svg>">
 endif
