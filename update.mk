be897ee533b6bb83ceddb7640d29c11bf5460665
Martin Thomson <mt@lowentropy.net>
2024-10-09 17:18:38 +1100

    Update updates

--- update.mk
+++ update.mk
@@ -68,25 +68,43 @@ for f in $(1); do \
 done
 endef
 
-.PHONY: update-readme update-codeowners update-files update-venue update-ci update-workflows
+.PHONY: update-readme update-codeowners update-makefile update-gitignore update-files update-venue update-ci update-workflows
 update-readme: auto_update
 	$(call regenerate,README.md)
 
 update-codeowners:
 	$(call regenerate,.github/CODEOWNERS)
 
-ifneq (true,$(CI))
-update-files: auto_update update-ci
-else
-update-files: auto_update
-endif
-	$(call regenerate,README.md Makefile .github/CODEOWNERS)
-	# .gitignore is fiddly and therefore requires special handling
+# We only need to copy over the rules that include and setup main.mk.
+# This keeps anything above the include where it is and moves everything else below these two things.
+# There is a tricky part in suppressing any blank line after `include <...>/main.mk`.
+# This uses 'x' and 'n' to get the next line, then conditionally prints a non-blank line.
+update-makefile: Makefile $(LIBDIR)/template/Makefile
+	@x=$$(mktemp); mv $< "$$x"; \
+	sed -n -e '1,/^include.*main\.mk$$/{x;1d;p;}' "$$x" > $<; \
+	sed -n -e '/^include.*main\.mk$$/,/^$$/p;/main\.mk:$$/,/^$$/p' $(LIBDIR)/template/Makefile >> $<; \
+	sed -n -e '1,/^include.*main\.mk$$/{/^include.*main\.mk$$/{x;n;/^$$/!p;};d;};/main\.mk:$$/,/^$$/d;p' "$$x" >> $<
+	@if ! git diff --quiet @ $<; then \
+	  git add $<; \
+	  git $(CI_AUTHOR) commit -m "Automatic update of $<"; \
+	fi
+
+# Re-run setup for .gitignore.
+update-gitignore:
 	$(MAKE) -f $(LIBDIR)/setup.mk CHECK_BRANCH=false setup-gitignore
 	@if ! git diff --quiet @ .gitignore; then \
 	  git add .gitignore; \
 	  git $(CI_AUTHOR) commit -m "Automatic update of .gitignore"; \
 	fi
+
+
+ifneq (true,$(CI))
+UPDATE_CI := update-ci
+else
+UPDATE_CI :=
+endif
+update-files: auto_update update-makefile update-gitignore $(UPDATE_CI)
+	$(call regenerate,README.md .github/CODEOWNERS)
 
 update-venue: auto_update $(drafts_source)
 	./$(LIBDIR)/update-venue.sh $(GITHUB_USER) $(GITHUB_REPO) $(filter-out auto_update,$^)
