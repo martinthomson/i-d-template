64b5ddff319ef2f6fcd85dccbb2d194c944e39af
Martin Thomson <mt@lowentropy.net>
2024-12-20 11:51:24 +1100

    Set PRE_SETUP for setup

--- docker/action/entrypoint.sh
+++ docker/action/entrypoint.sh
@@ -3,6 +3,9 @@ set -e
 git config --global --add safe.directory "$GITHUB_WORKSPACE"
 git config --global --add safe.directory "$GITHUB_WORKSPACE/./.git"
 
+if [ ! -f Makefile ]; then
+  export PRE_SETUP=true
+fi
 if [ "$1" = "setup" ]; then
   echo "Running setup"
   exec make -f lib/setup.mk
@@ -11,8 +14,7 @@ if [ ! -f Makefile ]; then
   echo "Cloning i-d-template into lib for default configuration."
   echo "Note: Until setup is complete, the editor's copy will not be updated."
   git clone https://github.com/martinthomson/i-d-template lib
-  echo "PRE_SETUP := true" >Makefile
-  cat lib/template/Makefile >>Makefile
+  cp lib/template/Makefile Makefile
 fi
 
 make .targets.mk
