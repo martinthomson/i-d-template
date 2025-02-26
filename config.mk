58ade019e4f0aa95c5b00f6527aa518d7aeda6c0
Martin Thomson <mt@lowentropy.net>
2025-02-26 17:10:31 +1100

    KRAMDOWN_REFCACHE_QUIET FTW

--- config.mk
+++ config.mk
@@ -104,6 +104,10 @@ export KRAMDOWN_REFCACHETTL_RFC := 23673600
 export KRAMDOWN_REFCACHETTL := 604800
 # Cache RFCs for 9 months since they are immutable.
 export KRAMDOWN_REFCACHETTL_DOI_IANA := 7776000
+ifneq (true,$(VERBOSE))
+# Suppress messages about downloading references.
+export KRAMDOWN_REFCACHE_QUIET := t
+endif
 endif
 XML2RFC_REFCACHEDIR ?= $(HOME)/.cache/xml2rfc
 KRAMDOWN_REFCACHEDIR := $(XML2RFC_REFCACHEDIR)
