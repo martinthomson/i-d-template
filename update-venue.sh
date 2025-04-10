9f12c5f3153b85f8625267cbd5e0741e2b8ce093
Richard Barnes <rlb@ipv.sx>
2025-04-10 15:33:18 -0400

    Use a more impossible character in sed commands

--- update-venue.sh
+++ update-venue.sh
@@ -34,13 +34,13 @@ s,^[# ]*latest: .*,  latest: "'"https://$user.github.io/$repo/${d%.*}.html"'",
 }' "$d"
     if [[ "$w" == "$last_wg"  ]] || wgmeta "$w"; then
         sed_no_backup -e '1,/^---/ {
-s,^[# ]*area: .*,area: "'"$wg_area"'",
-s,^[# ]*workgroup: .*,workgroup: "'"$wg_name"'",
+s!^[# ]*area: .*!area: "'"$wg_area"'"!
+s!^[# ]*workgroup: .*!workgroup: "'"$wg_name"'"!
 /^venue:/,/^[^# ]/{
-s,^[# ]*group: .*,  group: "'"$wg_name"'",
-s,^[# ]*type: .*,  type: "'"$wg_type"'",
-s,^[# ]*mail: .*,  mail: "'"$wg_mail"'",
-s,^[# ]*arch: .*,  arch: "'"$wg_arch"'",
+s!^[# ]*group: .*!  group: "'"$wg_name"'"!
+s!^[# ]*type: .*!  type: "'"$wg_type"'"!
+s!^[# ]*mail: .*!  mail: "'"$wg_mail"'"!
+s!^[# ]*arch: .*!  arch: "'"$wg_arch"'"!
 }
 }' "$d"
         last_wg="$w"
