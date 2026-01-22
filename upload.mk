ea35c94f85dab857fcfeaf045229f1ff1a3ec174
Martin Thomson <mt@lowentropy.net>
2026-01-22 17:47:08 +1100

    And quote $ properly

--- upload.mk
+++ upload.mk
@@ -38,9 +38,9 @@ comma := ,
 	  (head -1 "$@" | grep -q '^HTTP/\S\S* 20[01]\b' || { \
 	   $(if $(and $(TRACE_FILE),$(shell which jq 2>/dev/null)), \
 	       echo "$<" upload 1; \
-	       msg="$$(sed -ne '/^$/$(comma)$p' "$@" | jq -r '.error')"; \
+	       msg="$$(sed -ne '/^$$/$(comma)$$p' "$@" | jq -r '.error')"; \
 	       echo "$<" upload "Datatracker error: $${msg:-(unknown)}" >>"$(TRACE_FILE)"; \
-	       sed -ne '/^$/$(comma)$p' "$@" | jq -r '.messages[]' | while read -r line; do \
+	       sed -ne '/^$$/$(comma)$$p' "$@" | jq -r '.messages[]' | while read -r line; do \
 		 echo "$<" upload "$$line" >>"$(TRACE_FILE)"; \
 	       done \
 	     , cat "$@" 1>&2 \
